481f7b3f2d4e6116c15af5d5a794f0ce081af826
Megan Jacobs <mjay815@hotmail.com>
2017-09-18 21:01:39 -0400

    notes for planning added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
+// object for selecting the words
+
+// set up the variables for the game
+// the letters in the word, the letters guessed, the letters matching,
+
+// function setting up the game, picking the random word out of the object
+
+// check to see if the letter guessed matches, update page
 
 
 
@@ -11,6 +19,8 @@ $(document).ready(function() {
 
 
 
+
+// restart game, empty variables
 
 // ---------------------
 // end of document ready
